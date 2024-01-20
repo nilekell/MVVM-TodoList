29794eb1c6cf95cddaabfaca6e9d1b0f55921040
Nile Kelly <nile@NKs-MacBook-Pro.local>
2024-01-20 16:12:40 +0000

    Initial Commit

---
+++ MVVM-TodoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVM-TodoList
+//
+//  Created by Nile Kelly on 20/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVM-TodoList/MVVM_TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_TodoListApp.swift
+//  MVVM-TodoList
+//
+//  Created by Nile Kelly on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
